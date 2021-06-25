c178c108383ccd6d9285755ccb01dae8f908f7f9
Derek_Clemens <urwithinrange@yahoo.com>
2021-06-25 01:36:43 +0000

    Created file and wrote program into it

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,31 @@
+#inlcude <stdio.h>
+#inlcude <stdlib.h>
+#include <unistd.h>
+
+int main(void):
+{
+pid_t pid;
+int x;
+
+    for(x = 0; x < 5; x++)
+    {
+        pid = fork();
+        if (pid == 0)
+            exit(0);
+            printf("Zombie process created, PID: %d", pid)
+    }
+    infinite_while();
+    return (0);
+}
+
+
+
+
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
